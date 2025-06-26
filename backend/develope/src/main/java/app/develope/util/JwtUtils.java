2a45d7d6d2cc864ad005a83b12938791fc29023d
halwins <minhduc.fwrks@gmail.com>
2025-06-26 14:51:47 +0700

    Chuyển đổi từ war sang jar - 26/06/2025

--- backend/develope/src/main/java/app/develope/util/JwtUtils.java
+++ backend/develope/src/main/java/app/develope/util/JwtUtils.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
