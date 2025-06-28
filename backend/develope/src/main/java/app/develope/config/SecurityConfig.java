cf0696475b572781e5845887664cf734cdad3a68
halwins <minhduc.fwrks@gmail.com>
2025-06-28 11:32:16 +0700

    demo

--- backend/develope/src/main/java/app/develope/config/SecurityConfig.java
+++ backend/develope/src/main/java/app/develope/config/SecurityConfig.java
@@ -15,9 +15,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import app.develope.security.JwtAuthenticationFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
 
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
@@ -30,7 +32,7 @@ public class SecurityConfig{
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-         http.csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/v1/admin/**").hasAuthority("ADMIN")
@@ -38,7 +40,12 @@ public class SecurityConfig{
                         .anyRequest().authenticated())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(ex -> ex
+                        .accessDeniedHandler((req, res, ex1) -> {
+                            System.out.println("403 Access Denied - Role không đúng hoặc thiếu token");
+                            res.sendError(HttpServletResponse.SC_FORBIDDEN);
+                        }));
         return http.build();
     }
 
